e40128ee69cca0e0a390a0a5878df52e907437dd
Artem Oliinychenko <artem.oliinychenko@internetvikings.com>
2021-12-16 10:36:36 +0200

    lesson 35 updated

--- tbot.py
+++ tbot.py
@@ -35,8 +35,7 @@ about section and profile picture for your bot, see /help for a list of commands
 when you've finished creating your cool bot, ping our Bot Support if you want a better username for it. 
 Just make sure the bot is fully operational before you do this.
 
-Use this token to access the HTTP API:
-5044895054:AAEiNpf8Zvf9sQig_D-MkXjZYjy9AuOakGs
+
 Keep your token secure and store it safely, it can be used by anyone to control your bot.
 
 For a description of the Bot API, see this page: https://core.telegram.org/bots/api
